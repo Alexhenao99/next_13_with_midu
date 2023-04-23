0a834afb104ee9abb334472d2c611755fcb754fb
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-04-22 21:31:51 -0500

    Banner para dos compoonentes

---
+++ app/(with-banner)/layout.jsx
@@ -0,0 +1,10 @@
+const layout = ({ children }) => {
+  return (
+    <div>
+      <marquee style={{ background: '#fff', color: 'black' }}>Siguiendo el tutorial de Midu para aprender Next 13</marquee>
+      {children}
+    </div>
+  )
+}
+
+export default layout
